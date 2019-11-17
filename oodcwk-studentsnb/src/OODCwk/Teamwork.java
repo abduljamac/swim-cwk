8adcd18782b972df18b22146d90a468d23a59785
abdul <abduljama001@gmail.com>
2019-11-17 13:05:30 +0000

    uploaded the skeleton code that was provided
    Created all the classes for the Forces and Battles and started on the some of the functionalities of the game.

---
+++ oodcwk-studentsnb/src/OODCwk/Teamwork.java
@@ -0,0 +1,41 @@
+package OODCwk; 
+
+
+/**
+ * Details of your team
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Teamwork
+{
+    private String[] details = new String[7];
+    
+    public Teamwork()
+    {   // in each line replace the contents of the String 
+        // with the details of your team
+        // It will help us if the surname of programmer1 comes
+        // alphabetically before the surname of programmer2
+        details[0] = "Team Wolf";
+        details[1] = "Jamac";
+        details[2] = "Abdifatah";
+        details[3] = "16004006";
+        details[4] = "Nguru";
+        details[5] = "Ian Davis";
+        details[6] = "15059844";
+    }
+    
+    public String[] getTeamDetails()
+    {
+        return details;
+    }
+    
+    public void displayDetails()
+    {
+        for(String temp:details)
+        {
+            System.out.println(temp.toString());
+        }
+    }
+}
+        
